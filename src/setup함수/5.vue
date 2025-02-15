3b5b1a1a29c588df7fcee9c0c874c6f370c793b3
crinity-jaeygun <jaeygun@crinity.com>
2025-02-15 17:24:11 +0900

    first commit

---
+++ src/setup함수/5.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+	<div>{{ counter }}</div>
+	<div>{{ message }}</div>
+	<button @click="increase">increase counter</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		let counter = ref(0);
+		const message = ref('hello vue3');
+		const increase = () => {
+			counter.value++;
+		};
+		return {
+			counter,
+			message,
+			increase,
+		};
+	},
+};
+</script>
+
+<style></style>
