96bf00a61dd594549a6b2e4f82d2a1b6a83f0201
jeygeon <yoongeon.je@gmail.com>
2025-02-22 18:52:41 +0900

    v-for

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,30 @@
 	<div></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue';
+
+const num = ref(5);
+let result = computed(() => {
+	return num.value % 2 == 0 ? '짝수' : '홀수';
+});
+
+result = computed({
+	get() {
+		return num.value % 2 == 0 ? '짝수' : '홀수';
+	},
+	set(value) {
+		if (value == '짝수') {
+			num.value++;
+		} else {
+			num.value--;
+		}
+	},
+});
+
+console.log('변경전: ', num.value);
+result.value = '짝수';
+console.log('변경후: ', num.value);
+</script>
 
 <style lang="scss" scoped></style>
---
+++ src/v-for/1.vue
@@ -0,0 +1,63 @@
+<template>
+	<div>
+		<!--
+		목록을 출력할 때는 v-for 디렉티브를 통해서 렌더링 할 수 있다.
+		-->
+		<ul>
+			<li v-for="item in items" :key="item.id">
+				{{ item }}
+			</li>
+		</ul>
+
+		<hr />
+
+		<ul>
+			<li v-for="(item, index) in items" :key="item.id">
+				인덱스: {{ index }}, {{ item }}
+			</li>
+		</ul>
+
+		<hr />
+
+		<!--
+		만약에 이렇게 조건을 걸고싶으면 template을 사용하면 된다.
+		v-for와 v-if를 같은 태그에서 사용하는 것은 권장하지 않는다.
+		-->
+		<template v-for="item in items" :key="item.id">
+			<ul>
+				<li v-if="item.id % 2 === 0">{{ item }}</li>
+			</ul>
+		</template>
+
+		<hr />
+
+		<!--
+		객체의 속성을 반복해서 출력도 가능하다.
+		-->
+		<ul>
+			<li v-for="(value, key, index) in user" :key="key">
+				{{ key }} : {{ value }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+
+const items = ref([
+	{ id: 1, language: 'Java' },
+	{ id: 2, language: 'HTML' },
+	{ id: 3, language: 'CSS' },
+	{ id: 4, language: 'JavaScript' },
+	{ id: 5, language: 'Vue3' },
+]);
+
+const user = reactive({
+	name: 'hong',
+	age: 25,
+	location: 'seoul',
+});
+</script>
+
+<style lang="scss" scoped></style>
